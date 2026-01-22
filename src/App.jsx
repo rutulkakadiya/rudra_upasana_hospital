b8b5d790e738d489efacf757b2d91fd4071feaf6
rutulkakadiya21 <rutulkakadiya973@gmail.com>
2026-01-22 20:56:10 +0530

    Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import Home from './Components/Home/Home'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import WhatsApp from './Common Components/WhatsApp';
+import WhatsApp from './Common Components/Whatsapp.jsx';
 
 export default function App() {
 
